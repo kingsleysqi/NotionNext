f23c956a207a776f09ab1500c1b4be3c240a0687
Kingsley Qi <kingsley.s.qi@gmail.com>
2026-02-22 01:19:45 +0800

    更新 config.js

--- themes/plog/config.js
+++ themes/plog/config.js
@@ -5,9 +5,9 @@ const CONFIG = {
   NOBELIUM_MENU_TAG: true, // 显示标签
   NOBELIUM_MENU_ARCHIVE: false, // 显示归档
   NOBELIUM_MENU_SEARCH: true, // 显示搜索
-  NOBELIUM_MENU_RSS: true, // 显示订阅
+  NOBELIUM_MENU_RSS: false, // 显示订阅
 
-  NOBELIUM_NAV_NOTION_ICON: true // 是否读取Notion图标作为站点头像
+  NOBELIUM_NAV_NOTION_ICON: false, // 是否读取Notion图标作为站点头像
 
 }
 export default CONFIG
