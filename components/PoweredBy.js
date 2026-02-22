072979dfe9dbcca21aae195289c6ae4efce658a6
Kingsley Qi <kingsley.s.qi@gmail.com>
2026-02-22 10:42:25 +0800

    更新 PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -9,7 +9,7 @@ export default function PoweredBy(props) {
     <div className={`inline text-sm font-serif ${props.className || ''}`}>
       <span className='mr-1'>Powered by</span>
       <a
-        href='https://github.com/tangly1024/NotionNext'
+        href='kingsley.com'
         className='underline justify-start'>
         NotionNext {siteConfig('VERSION')}
       </a>
