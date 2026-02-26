b5ca7937bf79cf3cc933cf1e6872218f4e98e311
Kingsley Qi <kingsley.s.qi@gmail.com>
2026-02-27 04:51:03 +0800

    更新 blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -65,7 +65,7 @@ const BLOG = {
   // 欢迎语打字效果,Hexo,Matery主题支持, 英文逗号隔开多个欢迎语。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    'Hi，我骑着125cc穿越中国,Hi，我记录山路与夜色,Exit the logic, Enter the life,欢迎来到我的世界🎉',
+    '125cc穿越中国实践者,山河与夜色记录者,Exit the logic，Enter the life,欢迎来到我的世界🎉',
 
   // uuid重定向至 slug
   UUID_REDIRECT: process.env.UUID_REDIRECT || false
